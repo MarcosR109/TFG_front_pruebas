ac572b4033e37ba162d936004aac87d1772ec79d
Marcos Daniel Rodríguez <marcos.daniel@bosonit.com>
2025-02-12 13:54:31 +0100

    probando drag&drop

--- src/app/text-boxes/text-boxes.component.ts
+++ src/app/text-boxes/text-boxes.component.ts
@@ -1,41 +1,53 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // Necesario para ngModel
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button'; // Si usas botones
+import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import { Input } from '@angular/core';
 import { TextinputComponent } from '../textinput/textinput.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-text-boxes',
   standalone: true,
   templateUrl: './text-boxes.component.html',
   styleUrls: ['./text-boxes.component.css'],
   imports: [
-    FormsModule, // Para ngModel
-    MatCardModule, // Para mat-card
-    MatFormFieldModule, // Para mat-form-field
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
     MatInputModule,
     CommonModule,
     MatButtonModule,
     TextinputComponent,
+    DragDropModule,
   ],
 })
 export class TextBoxesComponent {
-  // 3 o 4 cuadrados vacíos
-  squares = [
-    { text: '' },
-    { text: '' },
-    { text: '' },
-    { text: '' }, // Puedes eliminar este si solo necesitas 3
-  ];
   lines: { letra: string }[] = [];
+  maxWidth: string = '100px'; // Ancho inicial mínimo
 
-  // Variable para almacenar el input del usuario
-  // Actualiza los cuadrados con el texto del usuario
   updateText(_$event: string[]): void {
     console.log('Updating text', _$event);
     this.lines = _$event.map((linea) => ({ letra: linea }));
+    this.updateMaxWidth();
+  }
+
+  updateMaxWidth(): void {
+    const charWidth = 9; // Ajustado para mejorar el tamaño
+    const minWidth = 100; // Mínimo tamaño en px
+    const maxWidth = 500; // Máximo tamaño en px
+
+    let newWidth = Math.max(
+      ...this.lines.map((l) => l.letra.length * charWidth),
+      minWidth
+    );
+    this.maxWidth = `${Math.min(newWidth, maxWidth)}px`;
+  }
+
+  onDrop(event: any, lineIndex: number, squareIndex: number): void {
+    const text = event.item.data;
+    this.lines[lineIndex].letra = this.lines[lineIndex].letra || '';
+    this.lines[lineIndex].letra += ` ${text}`;
   }
 }
