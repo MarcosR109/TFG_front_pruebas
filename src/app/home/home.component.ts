a0877a599002138d1fda9174d12aa0da05f24aef
Marcos Daniel Rodríguez <marcos.daniel@bosonit.com>
2025-04-29 13:56:44 +0200

    no me acuerdo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { MatCardContent } from '@angular/material/card';
 import { MatCardTitle } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../auth/auth.service';
+import { TrackByFunction } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [CommonModule, MatIcon, MatCardModule, MatButtonModule, RouterLink],
