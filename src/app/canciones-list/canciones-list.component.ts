d5968f958a11d0c26403fd73b662b27712d6993b
Marcos Daniel Rodríguez <marcos.daniel@bosonit.com>
2025-05-07 12:02:53 +0200

    agregada paginación en lista

--- src/app/canciones-list/canciones-list.component.ts
+++ src/app/canciones-list/canciones-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CancionService } from '../cancion.service';
 import { Cancion } from '../cancion/cancion';
 import { MatTableModule } from '@angular/material/table';
@@ -11,6 +11,11 @@ import { MatCard } from '@angular/material/card';
 import { MatIconButton } from '@angular/material/button';
 import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {
+  MatPaginator,
+  MatPaginatorModule,
+  PageEvent,
+} from '@angular/material/paginator';
 @Component({
   selector: 'app-canciones-list',
   imports: [
@@ -26,16 +31,19 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatSortModule,
     MatSort,
     MatProgressSpinnerModule,
+    MatPaginatorModule,
   ],
   templateUrl: './canciones-list.component.html',
   styleUrl: './canciones-list.component.css',
 })
 export class CancionesListComponent {
   cancionesFiltradas: any[] = [];
-
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   debug: any[] = [];
   cancionesInit!: Cancion[];
   isLoading: boolean = true;
+  pageSize = 10;
+  pageIndex = 0;
   constructor(private cancionService: CancionService) {}
 
   ngOnInit() {
@@ -45,7 +53,6 @@ export class CancionesListComponent {
       this.cancionesFiltradas = [...this.cancionesInit];
       this.isLoading = false;
     });
-
     this.cancionService.cancionesFiltradas$.subscribe((filtros) => {
       if (this.cancionesInit) {
         this.cancionesFiltradas = this.cancionService.filtrar(
@@ -54,6 +61,36 @@ export class CancionesListComponent {
         );
       }
     });
+  }
+  ngAfterViewInit() {
+    this.paginator.page.subscribe((event: PageEvent) => {
+      this.pageIndex = event.pageIndex;
+      this.pageSize = event.pageSize;
+      this.updatePaginated();
+    });
+    this.paginator._intl.itemsPerPageLabel = 'items por pagina';
+  }
+  updatePaginated() {
+    this.paginator.length = this.cancionesFiltradas.length;
+    const start = this.pageIndex * this.pageSize;
+    const end = start + this.pageSize;
+    this.cancionesFiltradas = this.cancionesInit.slice(start, end);
+    this.paginator._intl.getRangeLabel = function (
+      page: number,
+      pageSize: number,
+      length: number
+    ) {
+      if (length === 0 || pageSize === 0) {
+        return `0 de ${length}`;
+      }
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      const endIndex =
+        startIndex < length
+          ? Math.min(startIndex + pageSize, length)
+          : startIndex;
+      return `${startIndex + 1} - ${endIndex} de ${length}`;
+    };
   }
   sortData(sort: Sort) {
     const data = this.cancionesFiltradas.slice();
@@ -75,7 +112,15 @@ export class CancionesListComponent {
           return 0;
       }
     });
+    this.updatePaginated();
     this.cancionesFiltradas = this.debug;
+  }
+
+  pageChanged(event: PageEvent) {
+    this.cancionesFiltradas = this.cancionesInit.slice(
+      event.pageIndex * event.pageSize,
+      (event.pageIndex + 1) * event.pageSize
+    );
   }
 }
 function compare(a: number | string, b: number | string, isAsc: boolean) {
